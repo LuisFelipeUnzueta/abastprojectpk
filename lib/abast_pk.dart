9bbbbcb62ac3a4a03cfce89b46554f0ca1ddb2fc
LuisFelipeUnzueta <lfunzueta@gmail.com>
2024-06-23 18:08:20 -0300

    Abastecimento

--- lib/abast_pk.dart
+++ lib/abast_pk.dart
@@ -1,7 +1,38 @@
 library abast_pk;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Abastecimento {
+  String? id;
+  double valorTotal;
+  double quantidadeLitros;
+  int quilometragemRodada;
+
+  Abastecimento(
+    this.valorTotal,
+    this.quantidadeLitros,
+    this.quilometragemRodada,
+  );
+
+  double autonomia() {
+    return quilometragemRodada / quantidadeLitros;
+  }
+
+  double valorPorLitro() {
+    return valorTotal / quantidadeLitros;
+  }
+
+  double autonomiaMedia(List<Abastecimento> abastecimentos) {
+    double somatorio = 0.0;
+    int quant = abastecimentos.length;
+    for (var abast in abastecimentos) {
+      somatorio += abast.autonomia();
+    }
+    return somatorio / quant;
+  }
+
+  static Abastecimento fromMap(String id, Map<String, dynamic> map) {
+    var abast = Abastecimento(
+        map['valorTotal'], map['quantidadeLitros'], map['quilometragemRodada']);
+    abast.id = id;
+    return abast;
+  }
 }
